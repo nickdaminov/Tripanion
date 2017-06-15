71a6a789af5ed8ec11853ac8f00115d957340783
Duy Vu <dtv15@imperial.co.uk>
2017-06-15 16:07:05 +0100

    Remove anonymous func () with function() - forgot 1 last time

--- test/modelTest.js
+++ test/modelTest.js
@@ -5,7 +5,7 @@ var chai = require('chai');
 
 var assert = chai.assert;
 describe('User module', function() {
-  describe('"getUserById"', () => {
+  describe('"getUserById"', function() {
     it('should export a function', function() {
       // expect(User.getUserById).to.be.a('function')
       assert.equal(1, 1);
